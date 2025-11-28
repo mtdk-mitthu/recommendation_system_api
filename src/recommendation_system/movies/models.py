dd6e747ef7164c0758bab9308eea34e1281436a6
Mtdk.mitthu <mtdkmitthu@gmail.com>
2025-11-28 21:15:50 +0600

    Basic model

---
+++ src/recommendation_system/movies/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+class Movie(models.Model):
+    title = models.CharField(max_length=255)
+    genres = models.JSONField(default=list)
+    def __str__(self):
+        return self.title
